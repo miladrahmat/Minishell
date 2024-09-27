02d63e5a5221e994d08838fc365c85656e091de2
lemercie <lemercie@student.hive.fi>
2024-09-27 15:15:51 +0300

    free line

--- minishell.h
+++ minishell.h
@@ -6,13 +6,16 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 13:31:14 by lemercie          #+#    #+#             */
-/*   Updated: 2024/09/27 13:35:47 by lemercie         ###   ########.fr       */
+/*   Updated: 2024/09/27 15:12:25 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-# include <stdio.h>
+# include <stdio.h> // printf
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <stdbool.h> // bool type
+# include <stdlib.h> // malloc()
+# include "includes/libft/libft.h"
 #endif
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 11:35:34 by lemercie          #+#    #+#             */
-/*   Updated: 2024/09/27 14:17:05 by lemercie         ###   ########.fr       */
+/*   Updated: 2024/09/27 15:14:44 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,20 @@ int	main(void)
 {
 	char	*line;
 
-	line = readline("$>");
-	printf("%s\n", line);
+	while (true)
+	{
+		line = readline("$>");
+		if (line && *line)
+		{
+			if (ft_strncmp(line, "exit", 5) == 0)
+			{
+				free(line);
+				break ;
+			}
+			printf("%s\n", line);
+			add_history(line);
+		}
+		free(line);
+	}
 }
 
