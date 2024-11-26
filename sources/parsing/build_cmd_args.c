9c09fafe794e0be155a4ec6b17e784ddfc117d77
lemercie <lemercie@student.hive.fi>
2024-11-26 15:02:18 +0200

    split function

--- sources/parsing/build_cmd_args.c
+++ sources/parsing/build_cmd_args.c
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:27:47 by lemercie          #+#    #+#             */
-/*   Updated: 2024/11/21 19:26:06 by lemercie         ###   ########.fr       */
+/*   Updated: 2024/11/26 14:58:58 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,28 @@ static int	add_arguments(t_cmd *cmd)
 	return (0);
 }
 
+static int	create_not_builtin(t_cmd *cmd, t_env *env)
+{
+	cmd->path_error = 0;
+	cmd->cmd_args[0] = get_exec_path(
+			cmd->split_token->content, env, &cmd->path_error);
+	if (!cmd->cmd_args[0] && cmd->path_error == 0)
+		return (1);
+	if (!cmd->cmd_args[0] && cmd->path_error != 0)
+	{
+		cmd->cmd_args[0] = ft_strdup("");
+		if (!cmd->cmd_args[0])
+		{
+			cmd->path_error = 0;
+			return (1);
+		}
+	}
+	return (0);
+}
+
 // checks first token for a cmd, then just appends the rest of the tokens
 // if get_exec_path returns NULL AND path_error == 0: its a malloc() fail
 //	otherwise its command not found
-//
 int	build_cmd_args(t_cmd *cmd, t_env *env)
 {	
 	if (!cmd->split_token->content || ft_strlen(cmd->split_token->content) <= 0)
@@ -46,38 +64,17 @@ int	build_cmd_args(t_cmd *cmd, t_env *env)
 	}
 	cmd->cmd_args = malloc(sizeof(char *) * (ft_lstsize(cmd->split_token) + 1));
 	if (!cmd->cmd_args)
-	{
-//		printf("build_cmd_args(): A\n");
 		return (1);
-	}
-	if (!test_builtin_cmd(cmd->split_token->content))
-	{
-		cmd->path_error = 0;
-		cmd->cmd_args[0] = get_exec_path(
-				cmd->split_token->content, env, &cmd->path_error);
-		if (!cmd->cmd_args[0])
-		{
-			if (cmd->path_error == 0)
-			{
-//			printf("build_cmd_args(): B\n");
-			return (1);
-			}
-			cmd->cmd_args[0] = ft_strdup("");
-			if (!cmd->cmd_args[0])
-			{
-				cmd->path_error = 0;
-				return (1);
-			}
-		}
-	}
-	else
+	if (test_builtin_cmd(cmd->split_token->content))
 	{
 		cmd->cmd_args[0] = ft_strdup(cmd->split_token->content);
 		if (!cmd->cmd_args[0])
-		{
-//			printf("build_cmd_args(): C\n");
 			return (1);
-		}
+	}
+	else
+	{
+		if (create_not_builtin(cmd, env) > 0)
+			return (1);
 	}
 	return (add_arguments(cmd));
 }
